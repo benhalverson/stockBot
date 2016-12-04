d61421904cd77914a510c0f5c2a6219ade60716c
Ben Halverson <benhalverson33@gmail.com>
2016-12-03 16:41:48 -0800

    fix

--- main.ts
+++ main.ts
@@ -1,16 +1,63 @@
-/// <reference path="node.d.ts" />
-
-import util = require('util')
-
+import util = require('util');
+import request = require('request');
+const apikey = process.env.apikey || ''
+if(apikey === 'undefined') {
+  //throw Error 'Missing api key';
+}
 export class Bot {
-
   private args: any;
 
   constructor(args:Object){
     this.args = args;
   }
 
+  get extract(){
+    return "text";
+  }
+
   execute(cb:any){
-    throw "Nothing Implemented";
+    let args = this.args;
+    var relevance = 0.0;
+
+
+    var text: string = "AAPL";
+
+
+    for (let keyword of args.keywords){
+        if (parseFloat(keyword.relevance) > relevance){
+            relevance = keyword.relevance;
+            text = keyword.text;
+        }
+    }
+    let tradierurl: string = '';
+    let url = util.format(tradierurl,
+     apikey, encodeURIComponent(text));
+
+    request.get(url, (error, response, body)=>{
+      //TODO refactor for stock api
+      // curl -H "Authorization: Bearer {access token}" https://api.tradier.com/v1/markets/quotes?symbols=SPY,SPY140627C00195500
+
+        let photos = JSON.parse(body).photos;
+
+        if (photos && photos.photo.length > 0){
+          let photo = photos.photo[0];
+          let url = util.format("http://farm%s.staticflickr.com/%s/%s_%s.jpg", photo.farm, photo.server, photo.id, photo.secret);
+
+
+          let result = {
+            attachment: {
+              type: "image",
+              payload: {
+                url: url
+              }
+            }
+          };
+          cb(result);
+        } else {
+          cb({
+              text : util.format("Sorry could not find anything for \"%s\"", args.text)
+          });
+        }
+    });
   }
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+# stockBot
+* Uses the tradierAPI
+* facebook messenger bot that gets stock data from tradier.com
